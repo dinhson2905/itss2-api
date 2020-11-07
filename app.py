8093b353ee42ac2619d78b69543d007bc8aae51b
dinhson2905 <dinhson2905@gmail.com>
2020-11-07 17:12:40 +0700

    handle cors, modify data shops

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from resource import products, shops
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 class HelloWorld(Resource):
